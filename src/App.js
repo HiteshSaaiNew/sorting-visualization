b846a056fc496e885872b147ee320634dc6e0e47
Hitesh Saai Mananchery Panneerselvam <mppk2009@gmail.com>
2024-04-17 16:27:08 -0500

    push dev code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import SortingVisualization from './Components/SortingVisualization/SortingVisualization'
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualization> </SortingVisualization>
+    </div>
+  );
+}
+
+export default App;
